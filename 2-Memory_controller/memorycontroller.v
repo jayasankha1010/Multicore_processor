be2fc6f267b9dcd1d1e9af452379a959ec018afa
Chathura Jayasankha <jayasankha1010@gmail.com>
2021-07-02 16:46:35 +0530

    Memory controller implemented

--- 2-Memory_controller/memorycontroller.v
+++ 2-Memory_controller/memorycontroller.v
@@ -1,7 +1,8 @@
+//Author - Chathura Jayasankha Gamage
+
 module memorycontroller
 (
-input clk,
-input clkx,
+input clk16,
 
 input [11:0] addr0_,
 input we0_,
@@ -13,35 +14,37 @@ input we1_,
 input [15:0] dataIN1_,
 output reg [15:0] dataOUT1_,
 
-//input [11:0] addr2,
-//input we2,
-//input [15:0] dataIN2,
-//output [15:0] dataOUT2,
+input [11:0] addr2_,
+input we2_,
+input [15:0] dataIN2_,
+output reg [15:0] dataOUT2_,
 
-//input [11:0] addr3,
-//input we3,
-//input [15:0] dataIN3,
-//output [15:0] dataOUT3,
+input [11:0] addr3_,
+input we3_,
+input [15:0] dataIN3_,
+output reg [15:0] dataOUT3_,
 
-//input [11:0] addr4,
-//input we4,
-//input [15:0] dataIN4,
-//output [15:0] dataOUT4,
+input [11:0] addr4_,
+input we4_,
+input [15:0] dataIN4_,
+output reg [15:0] dataOUT4_,
 
-//input [11:0] addr5,
-//input we5,
-//input [15:0] dataIN5,
-//output [15:0] dataOUT5,
+input [11:0] addr5_,
+input we5_,
+input [15:0] dataIN5_,
+output reg [15:0] dataOUT5_,
 
-//input [11:0] addr6,
-//input we6,
-//input [15:0] dataIN6,
-//output [15:0] dataOUT6,
+input [11:0] addr6_,
+input we6_,
+input [15:0] dataIN6_,
+output reg [15:0] dataOUT6_,
 
-//input [11:0] addr7,
-//input we7,
-//input [15:0] dataIN7,
-//output [15:0] dataOUT7,
+input [11:0] addr7_,
+input we7_,
+input [15:0] dataIN7_,
+output reg [15:0] dataOUT7_,
+
+
 
 output reg [11:0] addr_,
 output reg we_,
@@ -50,32 +53,150 @@ input [15:0] dataOUT_
 
 );
 
-reg counter = 0;
+reg [2:0] counter = 0;
 
-always @(posedge clkx)
+always @(posedge clk16)
 begin
 
 counter<=counter+1;
 
 case(counter)
 
-	1'b0: 
+	3'd0: 
 		begin
 		addr_ <= addr0_;
 		we_ <= we0_;
 		dataIN_ <= dataIN0_;
 		dataOUT0_ <= dataOUT_;
 		end
-	1'b1: 
+	3'd1: 
 		begin
-		//if(addr1_>3500)
-		//addr_ <= addr1_+1;
-		//else
-		addr_ <= addr1_;
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+1;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
 		we_ <= we1_;
 		dataIN_ <= dataIN1_;
 		dataOUT1_ <= dataOUT_;
 		end
+		
+	3'd2: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+2;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+		
+	3'd3: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+3;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+		
+	3'd4: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+4;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+	
+	3'd4: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+4;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+		
+	3'd5: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+5;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+	
+	3'd6: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+6;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+		
+	3'd7: 
+		begin
+		if(addr1_>3499) 
+		begin
+			addr_ <= addr1_+7;
+		end
+		else
+		begin
+			addr_ <= addr1_;
+		end
+			
+		we_ <= we1_;
+		dataIN_ <= dataIN1_;
+		dataOUT1_ <= dataOUT_;
+		end
+		
 
 endcase
 end
